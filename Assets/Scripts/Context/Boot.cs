0d8959f5a72f9b1f14c0d7a909eee08521789489
KyryloAleksandrov <kyrylo.aleksandrov.98@gmail.com>
2024-11-19 15:47:26 +0100

    Project Context Added

---
+++ Assets/Scripts/Context/Boot.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Boot : MonoBehaviour
+{
+    void Awake()
+    {
+        ProjectContext.Instance.Initialize();
+    }
+
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForEndOfFrame();
+        SceneManager.LoadScene("Gameplay", LoadSceneMode.Single);
+    }
+}
